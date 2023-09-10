46019057ee5327bccafaa21c4b46afa6b0353da2
ordila <66573885+ordila@users.noreply.github.com>
2023-09-10 20:11:37 +0000

    Deploying to gh-pages from @ ordila/goit-js-hw-09@b83bee9ff71f6987daba11634de9c9b0da4aea9c 🚀

---
+++ 01-color-switcher.3edc5f04.js
@@ -0,0 +1,2 @@
+const t={buttonStart:document.querySelector("[data-start]"),buttonStop:document.querySelector("[data-stop]")};let o=null;t.buttonStart.addEventListener("click",(function(){o=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.buttonStart.disabled=!0})),t.buttonStop.addEventListener("click",(function(){clearInterval(o),t.buttonStart.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.3edc5f04.js.map
---
+++ 01-color-switcher.8e93b0d2.js
@@ -0,0 +1,2 @@
+!function(){var t={buttonStart:document.querySelector("[data-start]"),buttonStop:document.querySelector("[data-stop]")};var n=null;t.buttonStart.addEventListener("click",(function(){n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.buttonStart.disabled=!0})),t.buttonStop.addEventListener("click",(function(){clearInterval(n),t.buttonStart.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.8e93b0d2.js.map
